145fa1c560d324f6682ec689c855f08c11c68847
courjimen <coureyfrance@gmail.com>
2025-04-15 21:23:17 -0400

    updated hardcoded questions

--- server/server.js
+++ server/server.js
@@ -3,15 +3,40 @@ import cors from 'cors'
 // import pool from './db.js'
 
 const app = express()
-const port =  3000
+const port = 3000
 
 //mood quiz questions
 const moodQuestions = [
-{id: 1, text: 'How is your overall vibe today?'},
-{id: 2, text: 'In terms of energy level, are you feelilng like you are...'},
-{id: 3, text: 'What has your general outlook been like today?'},
-{id: 4, text: 'Are you feeling stressed or anxious at the moment?'},
-{id: 5, text: 'How is your headspace right now, is it...'},
+  {
+    id: 1,
+    text: 'How is your overall vibe today?',
+    options: ['Pretty awesome', 'A little down', 'Really not great'],
+    score: {'Pretty awesome': 2, 'A little down': 0, 'Really not great': -2}
+  },
+  { 
+    id: 2, 
+    text: 'In terms of energy level, are you feelilng like you are...',
+    options: ['Ready to tackle the world!', 'Just coasting', 'Totally running on empty'],
+    score: {'Ready to tackle the world!': 2, 'Just coasting': 0, 'Totally running on empty': -2} 
+  },
+  { 
+    id: 3, 
+    text: 'What has your general outlook been like today?',
+    options: ['Feeling really positive', 'Kind of neutral tbh', 'Honestly negative and down'],
+    score: {'Feeling really positive': 2, 'Kind of neutral tbh': 0, 'Honestly negative and down': -2}
+  },
+  { 
+    id: 4, 
+    text: 'Are you feeling stressed or anxious at the moment?',
+    options: ['Not at all', 'A little bit', 'Completely overwhelmed'],
+    score: {'Not at all': 2, 'A little bit': 0, 'Completely overwhelmed': -2}
+  },
+  { 
+    id: 5, 
+    text: 'How is your headspace right now, is it...',
+    options: ['Calm and clear', 'Restless and worried', 'Anxious and sad'],
+    score: {'Calm and clear': 2, 'Restless and worried': 0, 'Anxious and sad': -2}
+  },
 ]
 //write routes
 
@@ -19,5 +44,5 @@ app.use(express.json())
 app.use(cors())
 
 app.listen(port, () => {
-    console.log(`Server started on ${port}`)
+  console.log(`Server started on ${port}`)
 })

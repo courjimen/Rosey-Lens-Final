6bd89917fa21b09ba477e9a8798a41aa1b5ecaf4
courjimen <coureyfrance@gmail.com>
2025-04-15 21:07:39 -0400

    added 5 mood questions

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ import cors from 'cors'
 const app = express()
 const port =  3000
 
+//mood quiz questions
+const moodQuestions = [
+{id: 1, text: 'How is your overall vibe today?'},
+{id: 2, text: 'In terms of energy level, are you feelilng like you are...'},
+{id: 3, text: 'What has your general outlook been like today?'},
+{id: 4, text: 'Are you feeling stressed or anxious at the moment?'},
+{id: 5, text: 'How is your headspace right now, is it...'},
+]
 //write routes
 
 app.use(express.json())

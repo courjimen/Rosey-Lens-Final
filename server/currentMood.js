baa15b69271e1de987f3ba27ba28a6a64965661b
courjimen <coureyfrance@gmail.com>
2025-04-15 23:33:18 -0400

    correct format for mood data

--- server/currentMood.js
+++ server/currentMood.js
@@ -1,5 +1,25 @@
-const moodData = [
-    positive = [
-        
-    ]
-]
+const moodData = {
+  positive: [
+    "You're feeling so radiant.",
+    "You're feeling truly bright.",
+    "You're feeling such good energy.",
+    "You're feeling wonderfully positive.",
+    "You're feeling like pure sunshine."
+  ],
+  neutral: [
+    "You're feeling okay, and a little extra brightness is on its way.",
+    "You're feeling well, let's add some positive energy to your mood.",
+    "You're feeling alright, and a touch of joy is coming your way.",
+    "You're feeling neutral, grab a sprinkle of good vibes.",
+    "You're feeling bleh, here's a boost of positivity."
+  ],
+  negative: [
+    "You're feeling down. Let's change that.",
+    "You're feeling a bit low. Let's add some positivity.",
+    "You're feeling off. Let's change that.",
+    "You're feeling bad vibes. Let's get a moment of peace.",
+    "You're feeling less than your usual self. Let's change that."
+  ]
+}
+
+export default moodData
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import moodQuestions from './moodQuiz.js'
+import moodData from './currentMood.js'
 // import pool from './db.js'
 
 const app = express()
@@ -10,6 +11,11 @@ const port = 3000
 //GET QUESTIONS
 app.get('/question', (req, res) => {
   res.json(moodQuestions)
+})
+
+//GET Current Mood Data
+app.get('/mood', (req, res) => {
+  res.json(moodData)
 })
 
 app.use(express.json())

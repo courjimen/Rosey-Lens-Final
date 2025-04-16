f077a9afcb40e973a1ba0f8d76634ee12f56892b
courjimen <coureyfrance@gmail.com>
2025-04-15 23:15:20 -0400

    updated create account component

--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/CreateAccount.css'
 
 function CreateAccount() {
   return (
-    <div>CreateAccount</div>
+    <div>
+      <h2>CreateAccount</h2>
+      <div>
+      <form>
+        <input 
+        type='text'
+        placeholder='Username'
+        required
+        />
+        <input 
+        type='email'
+        placeholder='Email address'
+        required
+        />
+         <input 
+        type='phone'
+        placeholder='Phone'
+        
+        />
+        <input 
+        type='password'
+        placeholder='Password'
+        required
+        />
+        <input 
+        type='password'
+        placeholder='Confirm Password'
+        required
+        />
+        <button type='submit'>Create Account</button>
+      </form>
+      </div>
+
+      <Link to='/guest'>Return Home</Link>
+    </div>
   )
 }
 

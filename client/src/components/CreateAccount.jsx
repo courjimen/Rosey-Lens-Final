cad3c12b12196111499a2688f0df45db4045afdf
courjimen <coureyfrance@gmail.com>
2025-04-17 22:40:48 -0400

    add css

--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -4,44 +4,54 @@ import '../styles/CreateAccount.css'
 
 function CreateAccount() {
   return (
-    <div>
-      <h2>CreateAccount</h2>
-      <div>
-      <form>
-        <input 
-        type='text'
-        placeholder='Username'
-        required
-        />
-        <input 
-        type='email'
-        placeholder='Email address'
-        required
-        />
-         <input 
-        type='phone'
-        placeholder='Phone'
-        
-        />
-        <input 
-        type='password'
-        placeholder='Password'
-        required
-        />
-        <input 
-        type='password'
-        placeholder='Confirm Password'
-        required
-        />
+    <div className='create-account'>
+      <h2>Create Your Account</h2>
+      <div className='form-wrapper'>
+        <form className='input-form'>
+          <div>
+            <input
+              type='text'
+              placeholder='Username'
+              required
+            /></div>
 
-        <div>
-        <button type='submit'>Create Account</button>
-        </div>
-        
-      </form>
+          <div>
+            <input
+              type='email'
+              placeholder='Email address'
+              required
+            /></div>
+
+          <div>
+            <input
+              type='phone'
+              placeholder='Phone'
+            /></div>
+
+          <div>
+          <input
+            type='password'
+            placeholder='Password'
+            required
+          /></div>
+
+          <div>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            required
+          /></div>
+
+          <div>
+            <button type='submit'>Create Account</button>
+          </div>
+
+        </form>
       </div>
 
-      <Link to='/guest'>Return Home</Link>
+      <div className='link-container'>
+        <Link to='/guest'>Return Home</Link>
+      </div>
     </div>
   )
 }

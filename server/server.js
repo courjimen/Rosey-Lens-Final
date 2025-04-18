301ae1e72db9c9655bcf23b2ed6359cb2f400b18
courjimen <coureyfrance@gmail.com>
2025-04-18 17:37:18 -0400

    created post route for new accts

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import moodQuestions from './moodQuiz.js'
 import moodData from './currentMood.js'
-// import pool from './db.js'
+import pool from './db.js'
 
 const app = express()
 const port = 3000
 
+//POST NEW USER
+app.post('/new', async () => {
+  const {username, email, phone, password} = req.body
 
+  try {
+    const result = await pool.query('INSERT INTO user (username, email, phone, password) VALUES ($1, $2, $3, $4) RETURNING *')
+  }
+})
 //GET QUESTIONS
 app.get('/question', (req, res) => {
   res.json(moodQuestions)

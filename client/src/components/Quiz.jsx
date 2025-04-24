68a7d781550a88cc0309231ddb7fe74919e60201
courjimen <coureyfrance@gmail.com>
2025-04-23 23:46:32 -0400

    added fetching logic for questions

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Button, RadioGroup, Radio, FormControlledLLabel } from '@mui/material'
+import { Button, RadioGroup, Radio, FormControlLabel } from '@mui/material'
 import { Card, CardContent, CardHeader, CardTitle } from '@mui/material'
 import '../styles/Quiz.css'
 
@@ -8,6 +8,30 @@ function Quiz() {
   const [selectedAnswer, setSelectedAnswer] = useState('')
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/question')
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`)
+        }
+        const data = await res.json()
+        if (data && data.length > 0) {
+          setQuestionData(data[0])
+        } else {
+          setError('Error fetching questions')
+        }
+      } catch (error) {
+        setError(error)
+        console.error('Error fetching questions: ', error)
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+    fetchQuestion()
+  }, [])
   
   return (
     <div>Quiz</div>

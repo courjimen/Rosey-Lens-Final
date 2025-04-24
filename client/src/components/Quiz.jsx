a33e1a7e778d41c8d3dfa74550b879b1a0f71acb
courjimen <coureyfrance@gmail.com>
2025-04-23 23:21:31 -0400

    added use states

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Button, }
+import { Button, RadioGroup, Radio, FormControlledLLabel } from '@mui/material'
+import { Card, CardContent, CardHeader, CardTitle } from '@mui/material'
 import '../styles/Quiz.css'
 
 function Quiz() {
+  const [questionData, setQuestionData] = useState(null)
+  const [selectedAnswer, setSelectedAnswer] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  
   return (
     <div>Quiz</div>
   )

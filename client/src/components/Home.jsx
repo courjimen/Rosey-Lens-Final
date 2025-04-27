18409dc12ee6ded0399f5d85ee247f3ea4bdfbfa
courjimen <coureyfrance@gmail.com>
2025-04-27 01:40:15 -0400

    added middleware to server.js

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>Home Page</div>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ const port = 3000
 
 app.use(express.json())
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  next();
+});
 
 //POST NEW USER
 app.post('/new', async (req, res) => {

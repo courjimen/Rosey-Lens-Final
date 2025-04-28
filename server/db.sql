d1e249e6b52b360f1dcbe0d7654da1dbef917a3c
courjimen <coureyfrance@gmail.com>
2025-04-28 00:39:31 -0400

    updated db.sql

--- server/db.sql
+++ server/db.sql
@@ -1,17 +1,20 @@
 -- Table of Users
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY, 
-    username TEXT UNIQUE NOT NULL, 
-    email VARCHAR(125), 
-    phone VARCHAR(25), 
-    password TEXT NOT NULL);
-
+    firstname TEXT NOT NULL, 
+    lastname TEXT,
+    email VARCHAR(125) UNIQUE NOT NULL);
 --Table Quiz_Scores
-CREATE TABLE quiz (
+CREATE TABLE quiz_scores (
     id SERIAL PRIMARY KEY, 
-    user_id INT NOT NULL REFERENCES 
-    users(user_id), 
+    user_id INT NOT NULL REFERENCES users(user_id), 
     score INT NOT NULL, 
     date_completed TIMESTAMP WITH TIME ZONE DEFAULT NOW());
 
 --Table of Favorites
+CREATE TABLE favorites (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(user_id),
+    favorite_type VARCHAR(50) NOT NULL,
+    item_id INT NOT NULL,
+    UNIQUE (user_id, favorite_type, item_id));

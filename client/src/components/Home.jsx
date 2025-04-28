765b3a02e00afc6df8815b4d5467ebe044544f7c
courjimen <coureyfrance@gmail.com>
2025-04-27 21:58:43 -0400

    added more color

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,9 +5,10 @@ import roseImage from '../images/roseImage.webp'
 function Home() {
   return (
     <div className='home-page-container'>
-      <h1>Welcome to Rosy Lens</h1>
+      <h1>Welcome to <text className='red-text'>Rosy Lens</text></h1>
       <h2>Self care is in the air!</h2>
      <img className='rose' src={roseImage}/>
+     <h2>Based on your mood we give you positive uplifting affirmations, bible verses, or songs!</h2>
     </div>
   )
 }

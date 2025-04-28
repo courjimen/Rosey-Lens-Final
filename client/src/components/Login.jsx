1fd5b177505b78881558106bdf85c4cf07fa6268
courjimen <coureyfrance@gmail.com>
2025-04-28 10:40:13 -0400

    made suggested changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,30 +6,52 @@ import { jwtDecode } from 'jwt-decode'
 
 function Login() {
   const navigate = useNavigate()
+
+  const handleGoogleLoginSuccess = async (credentialResponse) => {
+    try {
+      const decoded = jwtDecode(credentialResponse.credential)
+      const firstName = decoded.given_name
+      const lastName = decoded.family_name
+      const email = decoded.email
+
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ firstname: firstName, lastname: lastName, email: email })
+      })
+
+      if (!response.ok) {
+        console.error('Error sending user data:', response.status)
+        navigate('/home', { state: { firstName: firstName } })
+        return
+      }
+
+      const data = await response.json()
+          console.log(data.message)
+          console.log('User data:', data.user)
+          navigate('/user', { state: { userId: data.user.user_id, firstName: firstName } })
+          
+          navigate('/quiz', { state: { userId: data.user.user_id, firstName: firstName } })
+
+        } catch (error) {
+        console.error('Error decoding token:', error)
+          navigate('/home', {state: {firstName: 'User' } })
+        }
+      }
+
+      const handleGoogleLoginFailure = (error) => {
+        console.log('Login failed', error)
+        navigate('/home', { state: { firstName: 'User' } })
+      }
+
+  
   return (
     <div className='login-container'>
       <h2>Sign in below:</h2>
-      <GoogleLogin onSuccess={(credentialResponse) => {
-        try {
-          const decoded = jwtDecode(credentialResponse.credential)
-          console.log(decoded)
-          const firstName = decoded.given_name
-          const lastName = decoded.family_name
-          const email = decoded.email
-          
-          navigate('/user', { state: { firstName} })
-          navigate('/quiz', { state: { firstName} })
-        } catch (error) {
-          console.error('Error decoding token:', error)
-          navigate('/home', { state: { firstName: 'User' } })
-        }
-      }}
-        onError={() => { 
-        console.log('Login failed')
-        navigate('/home', { state: { firstName: 'User' } })
-    }}
-      />
-</div>
+      <GoogleLogin onSuccess={handleGoogleLoginSuccess} onError={handleGoogleLoginFailure}/>
+    </div>
   )
 }
 
--- client/src/components/LoginHome.jsx
+++ client/src/components/LoginHome.jsx
@@ -6,16 +6,17 @@ import { Link, useLocation } from 'react-router-dom'
 function LoginHome() {
     const location = useLocation()
     const firstName = location.state?.firstName
+
     return (
       <div className='home-page-container'>
-        <h1>Welcome to <text className='red-text'>Rosy Lens</text>, {firstName}!</h1>
+        <h1>Welcome to <text className='red-text'>Rosy Lens</text> {firstName}</h1>
         <h2>Self care is in the air!</h2>
        <img className='rose' src={roseImage}/>
        <p className='description'>Based on your mood we give you positive <br/> uplifting affirmations, bible verses, or songs!</p>
        <div className='login-options'>
-       <p className='login'><Link to="/history" className='red-text'>View Mood History</Link></p>
-       <p className='guest'><Link to='/favorites' className='red-text'>View Favorites</Link></p>
-       <p className='quiz'><Link to='/quiz' className='red-text'>Check My Mood</Link></p>
+       <p className='login'><Link to="/history" className='green-text'>View Mood History</Link></p>
+       <p className='guest'><Link to='/favorites' className='green-text'>View Favorites</Link></p>
+       <p className='quiz'><Link to='/quiz' className='green-text'>Check My Mood</Link></p>
        </div>
       </div>
     )

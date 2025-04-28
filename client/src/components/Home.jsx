b3af9d2cd39f798beb66e4c5cd69c1c1ebbab83a
courjimen <coureyfrance@gmail.com>
2025-04-27 22:40:56 -0400

    updated css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,7 +8,11 @@ function Home() {
       <h1>Welcome to <text className='red-text'>Rosy Lens</text></h1>
       <h2>Self care is in the air!</h2>
      <img className='rose' src={roseImage}/>
-     <h2>Based on your mood we give you positive uplifting affirmations, bible verses, or songs!</h2>
+     <p className='description'>Based on your mood we give you positive <br/> uplifting affirmations, bible verses, or songs!</p>
+     <div className='login-options'>
+     <p className='login'><text className='red-text'>Login</text> with Google</p>
+     <p className='guest'>Continue as <text className='red-text'>Guest</text></p>
+     </div>
     </div>
   )
 }

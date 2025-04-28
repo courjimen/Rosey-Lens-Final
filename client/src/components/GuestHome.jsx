af369503db1bc0f7bc0ee7456363176bce459911
courjimen <coureyfrance@gmail.com>
2025-04-28 19:56:08 -0400

    added logic for guest home component

--- client/src/components/GuestHome.jsx
+++ client/src/components/GuestHome.jsx
@@ -7,23 +7,34 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 function GuestHome() {
     const [guestName, setGuestName] = useState('')
     const navigate = useNavigate()
-    
+
+    const handleNameChange = (e) => {
+        setGuestName(e.target.value)
+    }
+
+    const handleStartQuiz = (e) => {
+        e.preventDefault()
+        if (guestName.trim()) {
+            navigate('/quiz', { state: { firstName: guestName }})
+        } else {
+            alert('Please enter your name to start quiz')
+        }
+    }
+
     return (
         <div className='guest-home'>
-
             <div className='form-wrapper'>
             Please enter your name:
             <input 
             type='text'
-
+            value={guestName}
+            onChange={handleNameChange}
             />
             </div>
 
             <div>
             <button><Link to='/home'>Return Home</Link></button>
-            
-            <Link to='/quiz'><FontAwesomeIcon icon={faArrowRight}/></Link>
-            
+            <button onClick={handleStartQuiz}><FontAwesomeIcon icon={faArrowRight}/></button>
             </div>
         </div>
     )

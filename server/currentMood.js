2bb4bd7f27a051a670ac6574d601e06f1a4527b4
courjimen <coureyfrance@gmail.com>
2025-04-28 23:37:37 -0400

    added hardcoded affirmations

--- server/currentMood.js
+++ server/currentMood.js
@@ -27,15 +27,15 @@ const moodAffirmations = {
     "A radiant energy coming from you today is not only going to get you far, but inspire others to smile. Keep spreading the joy and good vibes!",
     "The kindness you radiate is a gift to the world, inspiring others to act with compassion and make a difference in the lives of those around them.",
     "Make each day your masterpiece. You have a kindness and positive vibe that will carry you to new heights this week! Keep being what you want to see in the world",
-    "You're feeling wonderfully positive.",
-    "You're feeling like pure sunshine."
+    "Keep showering yourself with a lot of love because you deserve it. Reflect on how far you've come and celebrate your wins!",
+    "Be your own kind of beautiful. Treat yourself with compassion and kindness, and let your inner light shine brightly for all to see."
   ],
   neutral: [
     "Your strength and resilience are a testament to your character, inspiring others to persevere through challenges and emerge even stronger.",
     "The way you navigate challenges with such grace is a testament to your inner fortitude, serving as a powerful example for those around you. Keep moving forward.",
-    "You're feeling alright, and a touch of joy is coming your way.",
-    "You're feeling neutral, grab a sprinkle of good vibes.",
-    "You're feeling bleh, here's a boost of positivity."
+    "Trust your path, you are exactly where you need to be. Have faith in the journey, for it is unfolding perfectly to guide you towards your destiny.",
+    "Embrace all that makes you, you. You're a gem in this world and you're meant to shine bright. Never let anyone or anything dim your light.",
+    "When you believe you can, in yourself, in your value...you're halfway there. Now is your time to shine."
   ],
   negative: [
     "You're feeling down. Let's change that.",

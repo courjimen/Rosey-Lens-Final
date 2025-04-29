d8d501c4bc6aa8fa9068f44c2d2f130b89b033d4
courjimen <coureyfrance@gmail.com>
2025-04-28 23:33:14 -0400

    hardcoding affirmations

--- server/currentMood.js
+++ server/currentMood.js
@@ -22,4 +22,30 @@ const moodData = {
   ]
 }
 
-export default moodData
+const moodAffirmations = {
+  positive: [
+    "A radiant energy coming from you today is not only going to get you far, but inspire others to smile. Keep spreading the joy and good vibes!",
+    "The kindness you radiate is a gift to the world, inspiring others to act with compassion and make a difference in the lives of those around them.",
+    "Make each day your masterpiece. You have a kindness and positive vibe that will carry you to new heights this week! Keep being what you want to see in the world",
+    "You're feeling wonderfully positive.",
+    "You're feeling like pure sunshine."
+  ],
+  neutral: [
+    "Your strength and resilience are a testament to your character, inspiring others to persevere through challenges and emerge even stronger.",
+    "The way you navigate challenges with such grace is a testament to your inner fortitude, serving as a powerful example for those around you. Keep moving forward.",
+    "You're feeling alright, and a touch of joy is coming your way.",
+    "You're feeling neutral, grab a sprinkle of good vibes.",
+    "You're feeling bleh, here's a boost of positivity."
+  ],
+  negative: [
+    "You're feeling down. Let's change that.",
+    "You're feeling a bit low. Let's add some positivity.",
+    "You're feeling off. Let's change that.",
+    "You're feeling bad vibes. Let's get a moment of peace.",
+    "You're feeling less than your usual self. Let's change that."
+  ]
+}
+
+
+
+export default moodData && moodAffirmations

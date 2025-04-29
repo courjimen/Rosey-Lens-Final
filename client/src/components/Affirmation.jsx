6fc76021cc63c456da8bf6b176f9f58d48aecca7
courjimen <coureyfrance@gmail.com>
2025-04-29 00:19:31 -0400

    added useState/location

--- client/src/components/Affirmation.jsx
+++ client/src/components/Affirmation.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import '../styles/Affirmation.css'
 
 function Affirmation() {
+  const [affirmation, setAffirmation] = useState('')
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const location = useLocation()
+  const moodCategory = location.state?.quizResult?.moodCategory
   return (
     <div>Affirmation</div>
   )

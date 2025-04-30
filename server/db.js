73538f6234f41f6e8116261145d0f320c7c8f238
courjimen <coureyfrance@gmail.com>
2025-04-30 12:01:24 -0400

    updated db.js

--- server/db.js
+++ server/db.js
@@ -1,12 +1,21 @@
 import pg from 'pg'
+import { Pool } from 'pg'
+
 
 //connect to database with credentials
-const pool = new pg.Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: 5432,
+// const pool = new pg.Pool({
+//     user: process.env.PGUSER,
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: 5432,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// })
+
+const pool = new Pool ({
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }

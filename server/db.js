1a9f812a4919fdbdc7b2cd1068fc78042e0f34a4
courjimen <coureyfrance@gmail.com>
2025-04-30 11:04:29 -0400

    fixed typos

--- server/db.js
+++ server/db.js
@@ -4,8 +4,8 @@ import pg from 'pg'
 const pool = new pg.Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
-    database: process.env.DATABASE,
-    port: process.env.PRT || 5432,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT || 5432,
     ssl: {
         rejectUnauthorized: false
     }

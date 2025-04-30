39bd6e9b154c36dd397290c30dd63b25eeea3505
courjimen <coureyfrance@gmail.com>
2025-04-30 11:13:15 -0400

    added PG password

--- server/db.js
+++ server/db.js
@@ -5,6 +5,7 @@ const pool = new pg.Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
     port: process.env.PGPORT || 5432,
     ssl: {
         rejectUnauthorized: false

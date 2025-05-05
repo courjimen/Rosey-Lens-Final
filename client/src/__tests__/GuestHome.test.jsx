3dd6bec2b5998b7293506ca1f873a8c0561d7c67
courjimen <coureyfrance@gmail.com>
2025-05-04 20:27:20 -0400

    added guest home test

--- client/src/__tests__/GuestHome.test.jsx
+++ client/src/__tests__/GuestHome.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import GuestHome from '../components/GuestHome.jsx'
 
 describe('Guest Home component renders', () => {
     it('renders GuestHome', () => {
         render(<GuestHome />)
+        expect(screen.getByRole('textbox')).toBeDefined()
     })
 })
